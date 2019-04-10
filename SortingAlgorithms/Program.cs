4f018c04d61896aa5e8719c4beebfae8d7262e2a
Krzysztof Ruczkowski <tojatos@gmail.com>
2019-04-10 20:09:15 +0200

    One case simulation user interaction

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -14,26 +14,25 @@ namespace SortingAlgorithms
             Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
             "SortingAlgorithms");
         
+        private static List<ISortingAlgorithm> _sortingAlgorithms = new List<ISortingAlgorithm> {
+            new Quicksort(),
+            new ShellSort(),
+            new LibrarySort(),
+        };
+        
         private static void Main()
         {
             var logger = new Logger();
             logger.OnLog += Console.WriteLine;
             var appData = new AppData(SavePath, logger);
-            var rng = new RandomNumbersGenerator(MinGeneratedValue, MaxGeneratedValue);
-            ISortingAlgorithm[] sortingAlgorithms = {
-                new Quicksort(),
-                new ShellSort(),
-                new LibrarySort(),
-            };
             while(true) {
                 Console.WriteLine("Welcome to the sorting algorithm simulation engine!");
                 Console.WriteLine($"Data path: {SavePath}");
                 Console.WriteLine("What do you want to do?");
-                Console.WriteLine("(1) - Start full simulation");
-                Console.WriteLine("(2) - Simulate one case");
-                Console.WriteLine("(3) - Exit");
+                Console.WriteLine("1 - Start full simulation");
+                Console.WriteLine("2 - Simulate one case");
+                Console.WriteLine("3 - Exit");
                 string k = Console.ReadLine();
-                Console.WriteLine(k);
 
                 switch (k)
                 {
@@ -57,10 +56,76 @@ namespace SortingAlgorithms
 
         private static void SimulateOneCase(AppData appData, Logger logger)
         {
-            Console.Clear();
-            Console.WriteLine("Not avaiable yet!");
+            ISortingAlgorithm selectedAlgorithm = SelectAlgorithm();
+            SequenceLength selectedLength = SelectLength();
+            SequenceType selectedType = SelectType();
+            Console.WriteLine($"Selected {selectedAlgorithm.Name}, {selectedLength.Description()}, {selectedType.Description()}");
+            
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
+        }
+
+        private static SequenceType SelectType()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please select sequence type:");
+                Console.WriteLine("1 - Random");
+                Console.WriteLine("2 - Half Sorted");
+                Console.WriteLine("3 - Sorted");
+                Console.WriteLine("4 - Reverse Sorted");
+                string k = Console.ReadLine();
+
+                switch (k)
+                {
+                    case "1": return SequenceType.Random;
+                    case "2": return SequenceType.HalfSorted;
+                    case "3": return SequenceType.Sorted;
+                    case "4": return SequenceType.ReverseSorted;
+                    default:
+                        continue;
+                }
+            }
+        }
+
+        private static SequenceLength SelectLength()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please select sequence length:");
+                Console.WriteLine("1 - Hundred Thousand");
+                Console.WriteLine("2 - Five Hundred Thousand");
+                Console.WriteLine("3 - Million");
+                Console.WriteLine("4 - Two Million");
+                string k = Console.ReadLine();
+
+                switch (k)
+                {
+                    case "1": return SequenceLength.HundredThousand;
+                    case "2": return SequenceLength.FiveHundredThousand;
+                    case "3": return SequenceLength.Million;
+                    case "4": return SequenceLength.TwoMillion;
+                    default:
+                        continue;
+                }
+            }
+        }
+
+        private static ISortingAlgorithm SelectAlgorithm()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please select sorting algorithm:");
+                for (int i = 0; i < _sortingAlgorithms.Count; i++)
+                    Console.WriteLine($"{i + 1} - {_sortingAlgorithms[i].Name}");
+                bool parsed = int.TryParse(Console.ReadLine(), out int k);
+                --k;
+                if (!parsed || _sortingAlgorithms.ElementAtOrDefault(k) == null) continue;
+                return _sortingAlgorithms[k];
+            }
         }
 
         private static void StartFullSimulation(AppData appData, Logger logger)
